ad488bc60d1dbba294427ff59495d64efe280058
GrahamHughesCode <grahamhughes@optonline.net>
2016-03-08 16:44:57 -0500

    add

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace test_1
             Console.BackgroundColor = ConsoleColor.Green;
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine("My poop was lovely");
+            Console.Beep();
             Console.ReadLine();
         }
     }
